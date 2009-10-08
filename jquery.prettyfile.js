1a6d0c50f9116d7f33bac6dc4de45978c4db21a9
mamuso <mamusino@gmail.com>
2009-10-09 00:54:32 +0200

    first plugin schema

---
+++ jquery.prettyfile.js
@@ -0,0 +1,17 @@
+/**
+* Pretty and easy file replacement
+*--------------------------------------------------------------------------*/
+$.fn.prettyfile = function(options){
+
+  var defaults = {
+    placeholder: "Browse files",
+		setvalue: 		true
+  };
+  var options = $.extend(defaults, opts||{});
+
+	// replace me baby
+  return this.each(function() {
+    var $self = this;
+  });
+  
+}
