f972bca6831433e5ddbce3437a808f52bf9a5757
“mamuso” <mamuso@mamuso.net>
2009-11-30 04:08:31 +0100

    some advances... sleep

--- jquery.prettyfile.js
+++ jquery.prettyfile.js
@@ -6,7 +6,9 @@ $.fn.prettyfile = function(options){
   var defaults = {
     setvalue: true,
     placeholderclass: "pf_placeholder",
-    placeholderdefault: "<span class='pf_placeholder'>Browse files</span>"
+    placeholderdefault: "<span class='pf_placeholder'>Browse files</span>",
+    placeholdercontclass: "pf_placeholder_cont",
+    wrapclass: "pf_wrap_class"
   };
   var options = $.extend(defaults, options||{});
 
@@ -14,12 +16,15 @@ $.fn.prettyfile = function(options){
   return this.each(function() {
     var $self = $(this),
         $wrap = document.createElement("span"),
-        $fakeinput = options.html !== undefined ? options.html : options.placeholderdefault;
-    $($wrap).css({"position":"relative", "overflow":"hidden", "display":"block", "overflow":"hidden"});
+        $fakeinput = document.createElement("span");
+
+    $($wrap).css({"position":"relative", "overflow":"hidden", "display":"block", "overflow":"hidden"}).attr("class", options.wrapclass);
+    $($fakeinput).append(options.html !== undefined ? options.html : options.placeholderdefault).attr("class", options.placeholdercontclass);
     
     $self.wrap($wrap);
     $self.attr("size", 1);
     $self.css({"position":"absolute", "right":"0", "font-size":"200em", "z-index":"2", "opacity":0}).after($fakeinput);
+    _fakeinput = $("."+ options.placeholdercontclass, $self.parent());
   });
 
 }
