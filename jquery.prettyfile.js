cd6332535716c6ff8c01eff9e0979411b694a0ce
“mamuso” <mamuso@mamuso.net>
2009-11-30 03:43:59 +0100

    advancing the concept

--- jquery.prettyfile.js
+++ jquery.prettyfile.js
@@ -1,17 +1,25 @@
 /**
-* Pretty and easy file replacement
+* Flexible file replacement
 *--------------------------------------------------------------------------*/
 $.fn.prettyfile = function(options){
 
   var defaults = {
-    placeholder: "Browse files",
-		setvalue: 		true
+    setvalue: true,
+    placeholderclass: "pf_placeholder",
+    placeholderdefault: "<span class='pf_placeholder'>Browse files</span>"
   };
-  var options = $.extend(defaults, opts||{});
+  var options = $.extend(defaults, options||{});
 
-	// replace me baby
+  // replace me baby
   return this.each(function() {
-    var $self = this;
+    var $self = $(this),
+        $wrap = document.createElement("span"),
+        $fakeinput = options.html !== undefined ? options.html : options.placeholderdefault;
+    $($wrap).css({"position":"relative", "overflow":"hidden", "display":"block", "overflow":"hidden"});
+    
+    $self.wrap($wrap);
+    $self.attr("size", 1);
+    $self.css({"position":"absolute", "right":"0", "font-size":"200em", "z-index":"2", "opacity":0}).after($fakeinput);
   });
-  
+
 }
